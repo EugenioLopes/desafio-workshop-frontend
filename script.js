f7c5c61e132a6900b07e8a25a9209757caa55461
EugenioLopes~ <eugeniolopes517@gmail.com~>
2022-03-26 10:15:52 -0300

    desafio-workshop-frontend

---
+++ script.js
@@ -0,0 +1,11 @@
+const copySelect = document.querySelector('[data-copy]')
+
+copySelect.addEventListener('click',copyToBoard)
+
+function copyToBoard()
+{ 
+    const copy = copySelect.dataset.copy
+    copySelect.classList.add('active')
+    navigator.clipboard.writeText(copy)
+    setTimeout(()=>{copySelect.classList.remove('active')},2300)
+}
